1b57ce0ada85cdf43415118f876b2fa135c388a2
= <jessur2020@gmail.com>
2020-05-19 15:50:21 -0400

    Add project files.

---
+++ Netbox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Netbox.Startup))]
+namespace Netbox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
